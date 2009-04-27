ffd39e06e5758035893303b269bb1f8a3c2d89ac
Klaus Kurzmann <mok@fluxnetz.de>
2009-04-27 14:29:17 +0200

    message-list: query *all* messages and not just the read ones
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/view/message-list-view.c
+++ src/view/message-list-view.c
@@ -114,7 +114,7 @@ void *message_list_view_show(struct Window *win, void *_options)
     window_swallow(win, "list", etk_embed_object_get(ETK_EMBED(data->container)));
 
 
-    ogsmd_sim_retrieve_messagebook("read", retrieve_messagebook_callback, data);
+    ogsmd_sim_retrieve_messagebook("all", retrieve_messagebook_callback, data);
     window_show(win);
 
     return data;
