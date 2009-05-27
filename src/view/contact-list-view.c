f8340cc7494e0c18982fdf5e7bcaa05d3d14b4c4
Jan Fechner <jf@jan-fechner.de>
2009-05-27 23:41:26 +0200

    lfp-efl2: Genlist in contacts view instead of elm_my_contactlist
    Hello,
    
    I've removed elm_my_contactlist to use elm_genlist as contacts list
    instead (the patch is attached to this mail).
    I think, everything "works" like before, but I dont really know how to
    theme the genlist to let it look like the elm_my_contactlist :(...
    Maybe someone want to try it though. :)
    
    Bye.
    
    PS: I'll look at the shr coding conventions now. ;)
    
    commit 864dcd427460f33257bcae18a68ea070a72a0042
    Author: agg1n <jan@ubuntu-jan.(none)>
    Date:   Wed May 27 22:17:50 2009 +0200
    
        use elm_genlist in contacts view instead of elm_my_contactlist
    
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/view/contact-list-view.c
+++ src/view/contact-list-view.c
@@ -11,6 +11,9 @@ struct ContactListViewData {
 };
 
 
+static Elm_Genlist_Item_Class itc;
+DBusGProxy *GQuery = NULL;
+
 
 static void 
 frame_list_show(void *_data);
@@ -61,6 +64,121 @@ contact_list_view_hide(void *_data)
 
 /* --- frame "list" --------------------------------------------------------- */
 
+char *
+gl_label_get(const void *data, Evas_Object *obj, const char *part)
+{
+	GHashTable *parameters = (GHashTable *)data;
+	char *label = NULL;
+
+	//g_debug("gl_label_get(part=%s)", part);
+
+	if (!strcmp(part, "elm.text"))
+		label = g_hash_table_lookup(parameters, "name");
+	else if (!strcmp(part, "elm.text.sub"))
+		label = g_hash_table_lookup(parameters, "number");
+
+	return strdup(label);
+}
+
+
+Evas_Object *
+gl_icon_get(const void *data, Evas_Object *obj, const char *part)
+{
+	return NULL;
+}
+
+
+Evas_Bool 
+gl_state_get(const void *data, Evas_Object *obj, const char *part)
+{
+	return 0;
+}
+
+
+void 
+gl_del(const void *data, Evas_Object *obj)
+{
+}
+
+
+static gint 
+_compare_entries(gconstpointer _a, gconstpointer _b)
+{
+	GHashTable **a = (GHashTable **)_a;
+	GHashTable **b = (GHashTable **)_b;
+	gpointer p; 
+	const char *name_a, *name_b;
+
+	p = g_hash_table_lookup(*a, "Name");
+	if (!p) {
+		name_a = "";
+		g_debug("name a not found!!!!");
+	} else
+		name_a = g_value_get_string(p);
+
+	p = g_hash_table_lookup(*b, "Name");
+	if (!p) {
+		name_b = "";
+		g_debug("name b not found!!!!");
+	} else
+		name_b = g_value_get_string(p);
+
+	return (strcasecmp(name_a, name_b));
+}
+
+static void 
+_process_entry(gpointer _entry, gpointer _data)
+{
+	GHashTable *entry = (GHashTable *)_entry;
+	struct ContactListViewData *data = (struct ContactListViewData *)_data;
+	const char *number = g_value_get_string(g_hash_table_lookup(entry, "Phone"));
+
+	if (number[0] == 't' && number[1] == 'e' && number[2] == 'l' && number[3] == ':')
+		number += 4;
+
+	g_debug("number is '%s'", number);
+
+	GHashTable *parameters = g_hash_table_new_full(g_str_hash, g_str_equal, free, free);
+	g_hash_table_insert(parameters, "path", strdup(g_value_get_string(g_hash_table_lookup(entry, "Path"))));
+	g_hash_table_insert(parameters, "name", strdup(g_value_get_string(g_hash_table_lookup(entry, "Name"))));
+	g_hash_table_insert(parameters, "number", strdup(number));
+
+	elm_genlist_item_append(data->list, &itc, parameters/*item data*/, NULL, ELM_GENLIST_ITEM_NONE, NULL, NULL);
+}
+
+static void 
+_retrieve_callback(GError *error, GPtrArray *messages, void *data)
+{
+	if (error != NULL || messages == NULL) {
+		g_debug("dbus error !!!");
+		return;
+	}
+
+	g_ptr_array_sort(messages, _compare_entries);
+	g_ptr_array_foreach(messages, _process_entry, data);
+}
+
+static void 
+_result_callback(GError *error, int count, void *data)
+{
+	if (error == NULL) {
+		g_debug("result gave %d entries --> retrieving", count);
+		opimd_contact_query_get_multiple_results(GQuery, count, _retrieve_callback, data);
+	}
+}
+
+static void 
+_query_callback(GError *error, char *query_path, void *data)
+{
+	if (error == NULL) {
+		g_debug("query path is %s", query_path);
+		GQuery = dbus_connect_to_opimd_contact_query (query_path);
+		opimd_contact_query_get_result_count (GQuery, _result_callback, data);
+	}
+}
+
+
+
 static void 
 frame_list_show(void *_data)
 {
@@ -71,8 +189,13 @@ frame_list_show(void *_data)
 
 	window_layout_set(win, CONTACTS_FILE, "list");
 
-	data->list = elm_my_contactlist_add(window_evas_object_get(data->win));
+	data->list = elm_genlist_add(window_evas_object_get(data->win));
 	window_swallow(data->win, "list", data->list);
+	itc.item_style     = "double_label";
+	itc.func.label_get = gl_label_get;
+	itc.func.icon_get  = gl_icon_get;
+	itc.func.state_get = gl_state_get;
+	itc.func.del       = gl_del;
 	evas_object_show(data->list);
 
 	data->bt1 = elm_button_add(window_evas_object_get(win));
@@ -126,6 +249,11 @@ frame_list_show(void *_data)
 	elm_box_pack_end(data->bx, data->bt_delete);
 
 	elm_hover_content_set(data->hv, "top", data->bx);
+
+	/* .. */
+	GHashTable *qry = g_hash_table_new_full(g_str_hash, g_str_equal, free, free);
+	opimd_contacts_query(qry, _query_callback, data);
+	g_hash_table_destroy(qry);
 }
 
 static void 
@@ -164,7 +292,9 @@ frame_list_call_clicked(void *_data, Evas_Object *obj, void *event_info)
 
 	g_debug("frame_list_call_clicked()");
 
-	GHashTable *properties = elm_my_contactlist_selected_row_get(data->list);
+	Elm_Genlist_Item *it = elm_genlist_selected_item_get(data->list);
+	GHashTable *properties = it ? elm_genlist_item_data_get(it) : NULL;
+
 	if (properties != NULL)
 		ogsmd_call_initiate(g_hash_table_lookup(properties, "number"), "voice", NULL, NULL);
 }
@@ -188,7 +318,9 @@ frame_list_message_clicked(void *_data, Evas_Object *obj, void *event_info)
 
 	evas_object_hide(data->hv);
 
-	GHashTable *properties = elm_my_contactlist_selected_row_get(data->list);
+	Elm_Genlist_Item *it = elm_genlist_selected_item_get(data->list);
+	GHashTable *properties = it ? elm_genlist_item_data_get(it) : NULL;
+
 	if (properties != NULL) {
 		assert(g_hash_table_lookup(properties, "number") != NULL);
 
@@ -210,7 +342,9 @@ frame_list_edit_clicked(void *_data, Evas_Object *obj, void *event_info)
 
 	evas_object_hide(data->hv);
 
-	GHashTable *properties = elm_my_contactlist_selected_row_get(data->list);
+	Elm_Genlist_Item *it = elm_genlist_selected_item_get(data->list);
+	GHashTable *properties = it ? elm_genlist_item_data_get(it) : NULL;
+
 	if (properties != NULL) {
 		GHashTable *options = g_hash_table_new(g_str_hash, g_str_equal);
 		g_hash_table_insert(options, "path", g_hash_table_lookup(properties, "path"));
@@ -234,7 +368,9 @@ frame_list_delete_clicked(void *_data, Evas_Object *obj, void *event_info)
 
 	evas_object_hide(data->hv);
 
-	GHashTable *properties = elm_my_contactlist_selected_row_get(data->list);
+	Elm_Genlist_Item *it = elm_genlist_selected_item_get(data->list);
+	GHashTable *properties = it ? elm_genlist_item_data_get(it) : NULL;
+
 	if (properties != NULL) {
 		GHashTable *options = g_hash_table_new(g_str_hash, g_str_equal);
 		g_hash_table_insert(options, "path", g_hash_table_lookup(properties, "path"));
@@ -259,6 +395,10 @@ static void
 frame_list_refresh_callback(struct ContactListViewData *data)
 {
 	g_debug("frame_list_refresh_callback");
-	elm_my_contactlist_refresh(data->list);
+	elm_genlist_clear(data->list);
+
+	GHashTable *qry = g_hash_table_new_full(g_str_hash, g_str_equal, free, free);
+	opimd_contacts_query(qry, _query_callback, data);
+	g_hash_table_destroy(qry);
 }
 
