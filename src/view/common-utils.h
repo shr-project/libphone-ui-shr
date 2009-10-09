504d8d1a850dae737e267be1bb2588d5e1d5aa75
Tom Hacohen <tom@nospam>
2009-10-09 18:41:14 +0200

    added more utility functnios

--- src/view/common-utils.h
+++ src/view/common-utils.h
@@ -1,5 +1,19 @@
 #ifndef _COMMON_UTILS_H
 #define _COMMON_UTILS_H
+#include <glib.h>
+
 char *
-common_utils_skip_tel_prefix(const char *string);
+common_utils_skip_prefix(const char *string, const char *prefix);
+char *
+common_utils_add_prefix(const char *_number, const char *prefix);
+
+GValue *
+common_utils_new_gvalue_string(const char *value);
+
+GValue *
+common_utils_new_gvalue_int(int value);
+
+GValue *
+common_utils_new_gvalue_boolean(int value);
+
 #endif
