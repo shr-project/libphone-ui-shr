057acda8089c6f31d0e790b9390bb6b76e8d415b
Klaus Kurzmann <mok@fluxnetz.de>
2009-10-16 23:02:15 +0200

    phonegui_init: remember argc and argv in init to use them in loop
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/phonegui-init.c
+++ src/phonegui-init.c
@@ -6,6 +6,8 @@
 #include <Elementary.h>
 #include <glib.h>
 
+int phonegui_argc;
+char **phonegui_argv;
 
 static gboolean
 _idle_foo(void *foo)
@@ -21,8 +23,11 @@ phonegui_backend_init(int argc, char **argv, int (*idle_cb) (void *))
 	setlocale(LC_ALL, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	bind_textdomain_codeset(PACKAGE, "UTF-8");
+	phonegui_argc = argc;
+	phonegui_argv = argv;
 
 	g_type_init();
+
 }
 
 void phonegui_backend_loop()
@@ -31,7 +36,7 @@ void phonegui_backend_loop()
 	GMainLoop *mainloop = g_main_loop_new(NULL, FALSE);
 
 	// Initializations
-	elm_init(argc, argv);
+	elm_init(phonegui_argc, phonegui_argv);
 	g_debug("Initiated elementary");
 
 	if (!ecore_main_loop_glib_integrate()) {
