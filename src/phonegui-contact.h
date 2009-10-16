7508d730d2c20b73c8a6967fc4d9c8eac076b225
Klaus Kurzmann <mok@fluxnetz.de>
2009-10-16 23:01:40 +0200

    remove bogus phonegui-contact
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/phonegui-contact.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PHONEGUI_CONTACT_H
-#define _PHONEGUI_CONTACT_H
-
-void phonegui_backend_contact_new_show(const char *name, const char *number);
-void phonegui_backend_contact_hide();
-
-#endif
