53f34c2a79b36ed67e905a427c4ab24a899764bf
Klaus Kurzmann <mok@fluxnetz.de>
2009-11-14 21:01:49 +0100

    phoneui_dialog: correct constness for type param
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/phoneui-dialog.c
+++ src/phoneui-dialog.c
@@ -8,13 +8,13 @@
 
 
 void
-phoneui_backend_dialog_show(int type)
+phoneui_backend_dialog_show(const int type)
 {
 	struct Window *win = window_new(D_("Information"));
 	instance_manager_add(INSTANCE_DIALOG, type, win);
 
 	GHashTable *options = g_hash_table_new(g_str_hash, g_str_equal);
-	g_hash_table_insert(options, "type", (gpointer) type);
+	g_hash_table_insert(options, "type", GINT_TO_POINTER(type));
 
 	window_init(win);
 	window_view_show(win, options, dialog_view_show, dialog_view_hide, NULL);
--- src/phoneui-dialog.h
+++ src/phoneui-dialog.h
@@ -1,6 +1,6 @@
 #ifndef _PHONEGUI_DIALOG_H
 #define _PHONEGUI_DIALOG_H
 
-void phoneui_backend_dialog_show(int type);
+void phoneui_backend_dialog_show(const int type);
 
 #endif
