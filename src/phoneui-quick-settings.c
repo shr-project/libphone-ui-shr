2ab49f9424772d23f7de57b8322ed4857bfc8bc1
Rui Miguel Silva Seabra <rms@1407.org>
2009-11-28 12:21:54 +0000

    Missing function, and use an else instead of returning void.

--- src/phoneui-quick-settings.c
+++ src/phoneui-quick-settings.c
@@ -17,10 +17,25 @@ phoneui_backend_quick_settings_show()
 	g_debug("phoneui_backend_quick_settings_show()");
 	if (win) {
 		window_show(win);
-		return;
+	} else {
+		win = window_new(D_("Quick Settings"));
+		window_init(win);
+		window_view_show(win, NULL, quick_settings_view_show, quick_settings_view_hide, _exit_cb);
 	}
-	win = window_new(D_("Dialer"));
-	window_init(win);
-	window_view_show(win, NULL, quick_settings_view_show, quick_settings_view_hide, _exit_cb);
+}
+
+
+void
+phoneui_backend_quick_settings_hide()
+{
+	g_debug("phoneui_backend_quick_settings_hide()");
+	if (win != NULL) {
+		window_destroy(win, NULL);
+		win = NULL;
+	} else {
+		win = window_new(D_("Quick Settings"));
+		window_init(win);
+		window_view_show(win, NULL, quick_settings_view_show, quick_settings_view_hide, _exit_cb);
+	}
 }
 
--- src/phoneui-quick-settings.h
+++ src/phoneui-quick-settings.h
@@ -2,5 +2,6 @@
 #define _PHONEGUI_QUICK_SETTINGS_H
 
 void phoneui_backend_quick_settings_show();
+void phoneui_backend_quick_settings_hide();
 
 #endif
