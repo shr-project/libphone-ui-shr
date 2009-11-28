a7d348c55eca1b80fb903fc0e12f01559ef4ef2f
Rui Miguel Silva Seabra <rms@1407.org>
2009-11-28 10:27:16 +0000

    Wrapper function.

---
+++ src/phoneui-quick-settings.c
@@ -0,0 +1,26 @@
+#include "phoneui-quick-settings.h"
+#include <glib.h>
+#include "window.h"
+#include "views.h"
+
+static struct Window *win = NULL;
+
+static void
+_exit_cb()
+{
+	win = NULL;
+}
+
+void
+phoneui_backend_quick-settings_show()
+{
+	g_debug("phoneui_backend_quick-settings_show()");
+	if (win) {
+		window_show(win);
+		return;
+	}
+	win = window_new(D_("Dialer"));
+	window_init(win);
+	window_view_show(win, NULL, quick_settings_view_show, quick_settings_view_hide, _exit_cb);
+}
+
---
+++ src/phoneui-quick-settings.h
@@ -0,0 +1,6 @@
+#ifndef _PHONEGUI_QUICK_SETTINGS_H
+#define _PHONEGUI_QUICK_SETTINGS_H
+
+void phoneui_backend_quick_settings_show();
+
+#endif
