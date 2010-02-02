944f13e2e90401179c49f98a04d66069ce36da3f
Klaus Kurzmann <mok@fluxnetz.de>
2010-02-02 19:49:00 +0100

    ui-utils: add TODO about fullscreen
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/util/ui-utils.c
+++ src/util/ui-utils.c
@@ -85,6 +85,7 @@ ui_utils_view_init(struct View *view, Elm_Win_Type type, const char *title,
 	elm_win_resize_object_add(view->win, view->layout);
 	evas_object_show(view->layout);
 
+	// TODO: use fullscreen ?
 	evas_object_resize(view->win, 480, 600);
 	view->show_cb = show_cb;
 	view->hide_cb = hide_cb;
@@ -92,7 +93,7 @@ ui_utils_view_init(struct View *view, Elm_Win_Type type, const char *title,
 	/* END of wtf */
 end:
 	return ret;
-	
+
 /* Error handling */
 free_bg:
 	evas_object_del(view->background);
