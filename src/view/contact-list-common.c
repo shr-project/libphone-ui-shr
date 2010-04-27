f969fd6aeba012263e67e39e8e38c38da2ae05f5
Tom 'TAsn' Hacohen <tom@nospam>
2010-04-27 12:14:48 +0300

    contact-list: fixed #1024 - scrolling to top is now possible with the index.

--- src/view/contact-list-common.c
+++ src/view/contact-list-common.c
@@ -239,7 +239,7 @@ contact_list_fill_index(struct ContactListData *list_data)
 	GHashTable *entry;
 	char *idx, *current_index = NULL;
 	char *name;
-	int index_count;
+	int init_index_count, index_count;
 	int new_index;
 	int height;
 
@@ -260,7 +260,8 @@ contact_list_fill_index(struct ContactListData *list_data)
 
 	evas_object_geometry_get(list_data->index, NULL, NULL, NULL, &height);
 	limit = height / index_button_height;
-	index_count = list_data->count / limit;
+	init_index_count = list_data->count / (limit - 1); /* The number of indexes excluding the first */
+	index_count = 0; /* Add the first as well */
 	it = elm_genlist_first_item_get(list_data->list);
 	while (it) {
 		entry = (GHashTable *)elm_genlist_item_data_get(it);
@@ -284,7 +285,7 @@ contact_list_fill_index(struct ContactListData *list_data)
 				elm_index_item_append(list_data->index,
 						      current_index,
 						      current_index_item);
-				index_count = list_data->count / limit;
+				index_count = init_index_count;
 			}
 			index_count--;
 		}
